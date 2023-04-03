648dfa867155180b8bab909b1490398606027ed7
Khidhir <khidhirakmal@outlook.com>
2023-04-03 21:25:19 +0800

    Canvas created

---
+++ app.js
@@ -0,0 +1,4 @@
+// Setting up Canvas //
+const canvas = document.getElementById("canvas"); // calls the ID and gives it a variable
+canvas.width = 200; // in pixels
+canvas.height = window.innerHeight; // height is according to browser size
