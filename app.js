2af546a59eba0dcbc3600c062344bb839d16ef8c
Khidhir <khidhirakmal@outlook.com>
2023-04-04 21:52:22 +0800

    Created Controls class for car movement

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 // Setting up Canvas //
 const canvas = document.getElementById("canvas"); // calls the ID and gives it a variable
 canvas.width = 200; // in pixels
-canvas.height = window.innerHeight; // height is according to browser size
+canvas.height = window.innerHeight; // height is set according to browser size
 
 // Get 2D Context //
 /*  <canvas> element does not provide drawing capabilities. 
@@ -9,19 +9,84 @@ canvas.height = window.innerHeight; // height is according to browser size
     once 2D context is obtained, we can use various methods and properties to draw shapes, text and images on the canvas. */
 const ctx = canvas.getContext("2d");
 
+// Assigning Controls //
+class Controls {
+  // constructor parameters left empty as there is no initial value or data to be passed in. proceed straight to functions for execution.
+  constructor() {
+    this.forward = false;
+    this.backward = false;
+    this.left = false;
+    this.right = false;
+    // in default state, none of the controls are active because all are set to "false".
+
+    this.#arrowKeyListeners(); // creates a private "#" method defined as arrowKeyListeners. private methods cannot be accessed outside of the class.
+  }
+
+  // an event listener waits for user interaction like a click or keypress and run codes based on that action
+  #arrowKeyListeners() {
+    document.onkeydown = (event) => {
+      // `onkeydown` is an event listener. `(event) => {}` means that a function will be executed when an event occurs.
+      switch (event.key) {
+        /* `switch` is a control statement that evaluates an expression `(event.key)` and executes a block of code (case) depending on the value 
+        of the expression. it will search through each `case` for a match and executes the block of code. 
+        
+        `event.key` is a property of `KeyboardEvent` object.
+        when the Up key is pressed, it becomes `switch (ArrowUp)`. switch will look for a case containing the same `KeyboardEvent` and executes the code block. */
+        case "ArrowUp":
+          this.forward = true;
+          break;
+        case "ArrowDown": // `case` specifies the possible value of expression being evaluated. "ArrowDown" is a `case` label. the colon `:` separates `case` label from codes
+          this.backward = true;
+          break; // terminates the execution of the `switch` statement code block and also marks the end of the code within `case` block.
+        case "ArrowLeft":
+          this.left = true;
+          break;
+        case "ArrowRight":
+          this.right = true;
+          break;
+      }
+      console.table(this); // a method that logs an array or object in a table format. it takes object as argument and display its properties and values in a tabular form.
+    };
+
+    document.onkeyup = (event) => {
+      // `onkeyup` is when the user stops pressing a key. when the `event` happens, it triggers the following callback function.
+      // when using `(event) => {}`, the `this` would refer to the constructor `this`. when using `function(event) {}`, `this` would only refer to within its own {}.
+      switch (event.key) {
+        case "ArrowUp":
+          this.forward = false;
+          break;
+        case "ArrowDown":
+          this.backward = false;
+          break;
+        case "ArrowLeft":
+          this.left = false;
+          break;
+        case "ArrowRight":
+          this.right = false;
+          break;
+      }
+      console.table(this);
+    };
+  }
+}
+
 // Creating Cars //
 class Car {
+  // 4 parameters which is required to create a rectangle path
   constructor(x, y, width, height) {
     this.x = x;
     this.y = y;
     this.width = width;
     this.height = height;
+    // "this" keyword refers to the instance being created and set its properties. e.g. new Car (100, 100, 30, 50)
+
+    this.controls = new Controls(); // creating a new Controls object and assigning it to "controls" property
   }
 
   draw(ctx) {
     ctx.beginPath(); // clears any existing path and begins a new path. a path is a sequence of points that is used to define a shape or a line.
     ctx.rect(
-      // used to define a rectangle path. takes in 4 arguments (x, y, width, height). this method does not draw, it only defines.
+      // .rect() used to define a rectangle path. takes in 4 arguments (x, y, width, height). this method does not draw, it only defines.
       this.x - this.width / 2, // x will be located at the center of the drawing
       this.y - this.width / 2, // y will be located at the center of the drawing
       this.width,
@@ -31,5 +96,5 @@ class Car {
   }
 }
 
-const car = new Car(100, 300, 30, 50); // new Car object (pos X, pos Y, width, height)
-car.draw(ctx);
+const raceCar = new Car(100, 300, 30, 50); // creating a new Car object (pos X, pos Y, width, height)
+raceCar.draw(ctx); // invoking draw (based on 2D context) for the raceCar
